4a8f488d412aaace6adec469b07b60e31082d38c
noy00y <69055108+noy00y@users.noreply.github.com>
2022-09-10 00:50:49 -0400

    yuh

--- client/src/Redux/index.js
+++ client/src/Redux/index.js
@@ -8,4 +8,7 @@ import {createStore} from 'redux';
 import reducers from './Reducers';
 
 // Create Store:
-const store = createStore(reducers);
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
