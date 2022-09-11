52ea7fb99d1737e5f7192f1139d7a37670f4edd3
noy00y <69055108+noy00y@users.noreply.github.com>
2022-09-11 02:20:44 -0400

    idefk

--- client/src/Views/Landing/LoginForm.js
+++ client/src/Views/Landing/LoginForm.js
@@ -1,4 +1,48 @@
 import React, {useState, setState} from 'react';
+import './LoginForm.scss';
+import $ from 'jquery'; 
+
+// Wtf:
+$(document).ready(function () {
+  $('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+    var $this = $(this),
+        label = $this.prev('label');
+  
+      if (e.type === 'keyup') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+          } else {
+            label.addClass('active highlight');
+          }
+      } else if (e.type === 'blur') {
+        if( $this.val() === '' ) {
+          label.removeClass('active highlight'); 
+        } else {
+          label.removeClass('highlight');   
+        }   
+      } else if (e.type === 'focus') {
+        
+        if( $this.val() === '' ) {
+          label.removeClass('highlight'); 
+        } 
+        else if( $this.val() !== '' ) {
+          label.addClass('highlight');
+        }
+      }
+  
+  });
+  
+  $('.tab a').on('click', function (e) {
+  
+    e.preventDefault();
+    
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+    
+    target = $(this).attr('href');
+  });
+});
 
 function Login() {
   // State:
@@ -7,12 +51,34 @@ function Login() {
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
 
+  // Input Handling:
+  const inputChange = (e) => {
+    const {id, value} = e.target;
+    if (id === "firstName") {
+      setFirstName(value);
+    }
+    if (id === "lastName") {
+      setLastName(value);
+    }
+    if (id === "email") {
+      setEmail(value);
+    }
+    if (id === "password") {
+      setPassword(value);
+    }
+  }
+
+  // Input Submitting:
+  const inputSubmit = () => {
+
+  }
+
   // HTML Return
   return (
     <div className = "form">
       <ul className = "tab-group">
-        <li className = "tab active"><a href = "#signup">Sign Up</a></li>
-        <li className = "tab"><a href = "#login">Sign Up</a></li>
+        <li className = "tab active"><a href = {"#signup"}>Sign Up</a></li>
+        <li className = "tab"><a href = {"#login"}>Log In</a></li>
       </ul>
 
       <div className = "tab-content">
